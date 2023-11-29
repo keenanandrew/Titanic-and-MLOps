47dea83644d123cdc8dc14f93a009c20794dedae
Andrew <keenanandrew@gmail.com>
2023-11-29 21:08:26 +0000

    Tidying and modularising

--- titanic.py
+++ titanic.py
@@ -1,22 +1,23 @@
 # A script for the Titanic dataset
 # For practicing mlflow, optuna and other MLOps libraries
 
+import mlflow
 import pandas as pd
-
-# preprocessing and analysis imports
-
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler, LabelEncoder
-
+from mlflow.models import infer_signature
 # from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import (accuracy_score, f1_score, precision_score,
+                             recall_score)
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+
+# preprocessing and analysis imports
+
+
 
 # mlflow imports
 
-import mlflow
-from mlflow.models import infer_signature
 
 # Import the data
 
