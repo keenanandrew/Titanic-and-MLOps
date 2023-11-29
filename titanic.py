def259ab34deb749890b9f7b6c6be34ff9644d44
Andrew <keenanandrew@gmail.com>
2023-11-29 21:47:39 +0000

    Tidying and modularising

--- titanic.py
+++ titanic.py
@@ -8,12 +8,11 @@ from mlflow.models import infer_signature
 # from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import (accuracy_score, f1_score, precision_score, recall_score)
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder, StandardScaler
 
 # preprocessing and analysis imports
-
 
 
 # mlflow imports
